dfe4db7a56e44604f8d57d21711cc49fd9e054f7
Narayansarawad <119670944+Narayansarawad@users.noreply.github.com>
2024-03-17 18:26:34 +0530

    Add files via upload

---
+++ international badmention.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void readarraymatchesplayers( int matches, int players  [][100]),
+if (i = 0 , i < n , i++)
+scanf ( "%d %d", &matches, &players );
+{
+    if (j=0,j<n,j++)
+        scanf("%d matches %d ,players", &matches ,&players  )
+
+}
+
+void printarraymatchesplayers( int matches, int players  [][100]),
+if (i = 0 , i < n , i++)
+printf ("%d = matches ,%d= players ")
+{
+    if (j=0,j<n,j++)
+        printf ("%d = matches ,%d= players ")
+
+}
+
