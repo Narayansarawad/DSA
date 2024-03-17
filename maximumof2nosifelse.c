874ec10af8d134a4588309a96b62ae0e351425c6
Narayansarawad <119670944+Narayansarawad@users.noreply.github.com>
2024-03-17 18:27:59 +0530

    Add files via upload

---
+++ maximumof2nosifelse.c
@@ -0,0 +1,24 @@
+//c program to calculate the distance between mohan and sohan and time
+/*Name:Narayan
+  Roll no:1761
+  Division:Q
+  Date:23/12/2022
+  */
+  #include<stdio.h>
+  #include<math.h>
+  int main()
+  {
+    int a,b;
+    printf("Enter a and b values\n");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+    {
+    printf("%d is maximum",a);
+    }
+    else
+    {
+    printf("%d is maximum",b);
+    }
+    return 0;
+  }
+
