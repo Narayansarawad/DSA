fde70cae0672144b5ebc6372921820e96fe48c3b
Narayansarawad <119670944+Narayansarawad@users.noreply.github.com>
2024-03-17 18:29:29 +0530

    Add files via upload

---
+++ peter problem.c
@@ -0,0 +1,16 @@
+//c program to calculate the distance travelled by audi car
+/*Name:Narayan
+  Roll no:1761
+  Division:Q
+  Date:23/12/2022
+  */
+  #include<stdio.h>
+  int main()
+  {
+      float miles,km;
+      printf("enter the value of distance in miles");
+      scanf("%f",&miles);
+      km=1.6*miles;
+      printf("km=%f",km);
+
+  }
