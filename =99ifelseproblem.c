02f107f36b5827cae95414a9ba18fa2158a0fb5d
Narayansarawad <119670944+Narayansarawad@users.noreply.github.com>
2024-03-17 18:05:57 +0530

    Add files via upload

---
+++ =99ifelseproblem.c
@@ -0,0 +1,22 @@
+/*Name:Narayan
+  Roll no:1761
+  Division:Q
+  Date:23/12/2022
+  */
+  #include<stdio.h>
+  #include<math.h>
+  int main()
+  {
+      int number;
+      printf("Enter the value of number\n");
+      scanf("%d",&number);
+      if(number==99)
+      {
+        printf("Number is 99\n");
+      }
+      else
+      {
+        printf("Number is not 99\n");
+      }
+     return 0;
+  }
