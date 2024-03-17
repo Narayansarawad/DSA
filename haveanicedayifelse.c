36cc2e3e5de680d9d344359cd17783c2fadb486e
Narayansarawad <119670944+Narayansarawad@users.noreply.github.com>
2024-03-17 18:25:57 +0530

    Add files via upload

---
+++ haveanicedayifelse.c
@@ -0,0 +1,18 @@
+/*Name:Narayan
+  Roll no:1761
+  Division:Q
+  Date:23/12/2022
+  */
+  #include<stdio.h>
+  #include<math.h>
+  int main()
+  {
+      char day;
+      printf("Enter the day\n");
+      scanf("%c",&day);
+      if(day=='friday'||day=='FRIDAY')
+        printf("Have a nice weekand\n");
+      else
+        printf("Have a nice day\n");
+      return 0;
+  }
